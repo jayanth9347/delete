44f8fe7db1410ce7c9bb1f71e8cb095f592ad9cf
jayanth <puajalajayanth9347@gmial.com>
2024-05-07 03:58:01 +0000

    commit message

---
+++ src/components/SimpleTodos/index.js
@@ -0,0 +1,46 @@
+const initialTodosList = [
+  {
+    id: 1,
+    title: 'Book the ticket for today evening',
+  },
+  {
+    id: 2,
+    title: 'Rent the movie for tomorrow movie night',
+  },
+  {
+    id: 3,
+    title: 'Confirm the slot for the yoga session tomorrow morning',
+  },
+  {
+    id: 4,
+    title: 'Drop the parcel at Bloomingdale',
+  },
+  {
+    id: 5,
+    title: 'Order fruits on Big Basket',
+  },
+  {
+    id: 6,
+    title: 'Fix the production issue',
+  },
+  {
+    id: 7,
+    title: 'Confirm my slot for Saturday Night',
+  },
+  {
+    id: 8,
+    title: 'Get essentials for Sunday car wash',
+  },
+]
+
+const SimpleTodos = props => {
+  const {initialTodosList} = props 
+  const {id, title} = initialTodosList
+  return (
+    <div>
+      <h1> hi hello </h1>
+    </div>
+  )
+}
+
+export default SimpleTodos
